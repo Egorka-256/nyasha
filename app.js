0235bf74e23fa3ba2e07beb02653dc318ea204ca
Egorka-256 <132396402+Egorka-256@users.noreply.github.com>
2024-07-08 02:53:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,57 @@
-const { Telegraf } = require("telegraf");
-const { message } = require("telegraf/filters");
-const axios = require("axios");
-require("dotenv").config()
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+const axios = require('axios');
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.token);
-bot.start((ctx) => ctx.reply("Привет, пупсик! Я отправлю тебе любую девочку, которую ты выберешь."));
+
+bot.start((ctx) => ctx.reply('Привет, пупсик! Я отправлю тебе любую девочку, которую ты выберешь.'));
+
 bot.help(async (ctx) => {
-    ctx.reply(
-      "/sfw - отправляет тянку без хентая\n" +
-      "/nsfw - отправляет хентай тянку\n" +
-      "/neko - отправляет хентай кошкадевочку\n" +
-      "/trap - отправляет хентай девочку с подвохом\n" +
-      "/blowjob - отправляет хентай девочку с чупачупсом"
-    );
-  });
-bot.command("hello", (ctx) => {
-  ctx.reply("hello world!");
-  console.log("hello world");
+  ctx.reply(
+    '/sfw - отправляет тянку без хентая\n'
+    + '/nsfw - отправляет хентай тянку\n'
+    + '/neko - отправляет хентай кошкадевочку\n'
+    + '/trap - отправляет хентай девочку с подвохом\n'
+    + '/blowjob - отправляет хентай девочку с чупачупсом'
+  );
 });
-bot.command("sfw", async (ctx) => {
-  try {
-    const response = await axios.get("https://api.waifu.pics/sfw/waifu");
-    const imageUrl = response.data.url;
 
+bot.command('hello', (ctx) => {
+  ctx.reply('hello world!');
+  console.log('hello world');
+});
+
+const getSFWWaifuImage = async (ctx) => {
+  try {
+    const response = await axios.get('https://api.waifu.pics/sfw/waifu');
+    const imageUrl = response.data.url;
     await ctx.replyWithPhoto({ url: imageUrl });
   } catch (error) {
-    console.error("Error fetching waifu image:", error);
-    await ctx.reply("Извините, произошла ошибка при получении картинки.");
+    console.error('Error fetching waifu image:', error);
+    await ctx.reply('Извините, произошла ошибка при получении картинки.');
   }
-});
+};
 
-bot.command("nsfw", async (ctx) => {
+const getNSFWWaifuImage = async (ctx, endpoint) => {
   try {
-    const response = await axios.get("https://api.waifu.pics/nsfw/waifu");
+    const response = await axios.get(`https://api.waifu.pics/nsfw/${endpoint}`);
     const imageUrl = response.data.url;
-
     await ctx.replyWithPhoto({ url: imageUrl });
   } catch (error) {
-    console.error("Error fetching waifu image:", error);
-    await ctx.reply("Извините, произошла ошибка при получении картинки.");
+    console.error('Error fetching waifu image:', error);
+    await ctx.reply('Извините, произошла ошибка при получении картинки.');
   }
-});
+};
 
-bot.command("neko", async (ctx) => {
-  try {
-    const response = await axios.get("https://api.waifu.pics/nsfw/neko");
-    const imageUrl = response.data.url;
+bot.command('sfw', getSFWWaifuImage);
+bot.command('nsfw', (ctx) => getNSFWWaifuImage(ctx, 'waifu'));
+bot.command('neko', (ctx) => getNSFWWaifuImage(ctx, 'neko'));
+bot.command('trap', (ctx) => getNSFWWaifuImage(ctx, 'trap'));
+bot.command('blowjob', (ctx) => getNSFWWaifuImage(ctx, 'blowjob'));
 
-    await ctx.replyWithPhoto({ url: imageUrl });
-  } catch (error) {
-    console.error("Error fetching waifu image:", error);
-    await ctx.reply("Извините, произошла ошибка при получении картинки.");
-  }
-});
-
-bot.command("trap", async (ctx) => {
-  try {
-    const response = await axios.get("https://api.waifu.pics/nsfw/trap");
-    const imageUrl = response.data.url;
-
-    await ctx.replyWithPhoto({ url: imageUrl });
-  } catch (error) {
-    console.error("Error fetching waifu image:", error);
-    await ctx.reply("Извините, произошла ошибка при получении картинки.");
-  }
-});
-
-bot.command("blowjob", async (ctx) => {
-  try {
-    const response = await axios.get("https://api.waifu.pics/nsfw/blowjob");
-    const imageUrl = response.data.url;
-
-    await ctx.replyWithPhoto({ url: imageUrl });
-  } catch (error) {
-    console.error("Error fetching waifu image:", error);
-    await ctx.reply("Извините, произошла ошибка при получении картинки.");
-  }
-});
-
-console.log("bot started");
+console.log('bot started');
 bot.launch();
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
